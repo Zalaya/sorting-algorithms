f55f9d5e4574f5b9803f004bc9596689774d9e15
Zalaya <enol.corzal@gmail.com>
2024-10-04 17:50:46 +0200

    feat(algorithms): add some sorting algorithms

---
+++ src/main/java/xyz/zalaya/sorting/algorithms/implementations/TimSortingAlgorithm.java
@@ -0,0 +1,12 @@
+package xyz.zalaya.sorting.algorithms.implementations;
+
+import xyz.zalaya.sorting.algorithms.SortingAlgorithm;
+
+public class TimSortingAlgorithm implements SortingAlgorithm {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] unsortedArray) {
+        return null;
+    }
+
+}
