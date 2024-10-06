9dbb45b067e7344f0b04203fb186aaab8ec9a21a
Zalaya <enol.corzal@gmail.com>
2024-10-06 03:05:48 +0200

    refactor(interface): change the default method to sort lists

--- src/main/java/xyz/zalaya/sorting/core/SortingAlgorithm.java
+++ src/main/java/xyz/zalaya/sorting/core/SortingAlgorithm.java
@@ -1,15 +1,13 @@
 package xyz.zalaya.sorting.core;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface SortingAlgorithm {
 
-    <T extends Comparable<T>> T[] sort(T[] unsortedArray);
+    <T extends Comparable<T>> T[] sort(T[] array);
 
-    @SuppressWarnings("unchecked")
-    default <T extends Comparable<T>> List<T> sort(List<T> unsortedList) {
-        return Arrays.asList(sort(unsortedList.toArray((T[]) new Comparable[unsortedList.size()])));
+    default <T extends Comparable<? super T>> List<T> sort(List<T> list) {
+        return list.stream().sorted().toList();
     }
 
 }
