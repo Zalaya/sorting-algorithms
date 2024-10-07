09359121d58aca81634c3e70f0326fd063b516b3
Zalaya <enol.corzal@gmail.com>
2024-10-07 18:27:03 +0200

    refactor(actions): change the action, now it only tests and change the tests

--- src/test/java/xyz/zalaya/sorting/core/SortingAlgorithmTest.java
+++ src/test/java/xyz/zalaya/sorting/core/SortingAlgorithmTest.java
@@ -1,10 +1,6 @@
 package xyz.zalaya.sorting.core;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -12,122 +8,219 @@ public abstract class SortingAlgorithmTest {
 
     public abstract SortingAlgorithm getSortingAlgorithm();
 
-    private static Stream<Arguments> getIntegerArrays() {
-        return Stream.of(
-            Arguments.of(new Integer[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 }, new Integer[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 }),
-            Arguments.of(new Integer[] { }, new Integer[] { }),
-            Arguments.of(new Integer[] { 1 }, new Integer[] { 1 }),
-            Arguments.of(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Integer[] { Integer.MAX_VALUE, 0, Integer.MIN_VALUE }, new Integer[] { Integer.MIN_VALUE, 0, Integer.MAX_VALUE }),
-            Arguments.of(new Integer[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new Integer[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }),
-            Arguments.of(new Integer[] { -1, -2, -3, -4, -5, -6, -7, -8, -9 }, new Integer[] { -9, -8, -7, -6, -5, -4, -3, -2, -1 })
-        );
+    @Test
+    public void shouldSortEmptyIntegerArray() {
+        Integer[] unsortedArray = new Integer[] { };
+        Integer[] expectedSortedArray = new Integer[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getDoubleArrays() {
-        return Stream.of(
-            Arguments.of(new Double[] { 3.0, 1.0, 4.0, 1.0, 5.0, 9.0, 2.0, 6.0, 5.0 }, new Double[] { 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 6.0, 9.0 }),
-            Arguments.of(new Double[] { }, new Double[] { }),
-            Arguments.of(new Double[] { 1.0 }, new Double[] { 1.0 }),
-            Arguments.of(new Double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 }, new Double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 }),
-            Arguments.of(new Double[] { 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0 }, new Double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 }),
-            Arguments.of(new Double[] { Double.MAX_VALUE, 0.0, -Double.MAX_VALUE }, new Double[] { -Double.MAX_VALUE, 0.0, Double.MAX_VALUE }),
-            Arguments.of(new Double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 }, new Double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 }),
-            Arguments.of(new Double[] { -1.0, -2.0, -3.0, -4.0, -5.0, -6.0, -7.0, -8.0, -9.0 }, new Double[] { -9.0, -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0 })
-        );
+    @Test
+    public void shouldSortSingleIntegerArray() {
+        Integer[] unsortedArray = new Integer[] { 1 };
+        Integer[] expectedSortedArray = new Integer[] { 1 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getByteArrays() {
-        return Stream.of(
-            Arguments.of(new Byte[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 }, new Byte[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 }),
-            Arguments.of(new Byte[] { }, new Byte[] { }),
-            Arguments.of(new Byte[] { 1 }, new Byte[] { 1 }),
-            Arguments.of(new Byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new Byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Byte[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new Byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Byte[] { Byte.MAX_VALUE, 0, Byte.MIN_VALUE }, new Byte[] { Byte.MIN_VALUE, 0, Byte.MAX_VALUE }),
-            Arguments.of(new Byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new Byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }),
-            Arguments.of(new Byte[] { -1, -2, -3, -4, -5, -6, -7, -8, -9 }, new Byte[] { -9, -8, -7, -6, -5, -4, -3, -2, -1 })
-        );
+    @Test
+    public void shouldSortUnsortedIntegerArray() {
+        Integer[] unsortedArray = new Integer[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+        Integer[] expectedSortedArray = new Integer[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getShortArray() {
-        return Stream.of(
-            Arguments.of(new Short[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 }, new Short[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 }),
-            Arguments.of(new Short[] { }, new Short[] { }),
-            Arguments.of(new Short[] { 1 }, new Short[] { 1 }),
-            Arguments.of(new Short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new Short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Short[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new Short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
-            Arguments.of(new Short[] { Short.MAX_VALUE, 0, Short.MIN_VALUE }, new Short[] { Short.MIN_VALUE, 0, Short.MAX_VALUE }),
-            Arguments.of(new Short[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new Short[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }),
-            Arguments.of(new Short[] { -1, -2, -3, -4, -5, -6, -7, -8, -9 }, new Short[] { -9, -8, -7, -6, -5, -4, -3, -2, -1 })
-        );
+    @Test
+    public void shouldSortEmptyDoubleArray() {
+        Double[] unsortedArray = new Double[] { };
+        Double[] expectedSortedArray = new Double[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getLongArrays() {
-        return Stream.of(
-            Arguments.of(new Long[] { 3L, 1L, 4L, 1L, 5L, 9L, 2L, 6L, 5L }, new Long[] { 1L, 1L, 2L, 3L, 4L, 5L, 5L, 6L, 9L }),
-            Arguments.of(new Long[] { }, new Long[] { }),
-            Arguments.of(new Long[] { 1L }, new Long[] { 1L }),
-            Arguments.of(new Long[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L }, new Long[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L }),
-            Arguments.of(new Long[] { 9L, 8L, 7L, 6L, 5L, 4L, 3L, 2L, 1L }, new Long[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L }),
-            Arguments.of(new Long[] { Long.MAX_VALUE, 0L, Long.MIN_VALUE }, new Long[] { Long.MIN_VALUE, 0L, Long.MAX_VALUE }),
-            Arguments.of(new Long[] { 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L }, new Long[] { 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L }),
-            Arguments.of(new Long[] { -1L, -2L, -3L, -4L, -5L, -6L, -7L, -8L, -9L }, new Long[] { -9L, -8L, -7L, -6L, -5L, -4L, -3L, -2L, -1L })
-        );
+    @Test
+    public void shouldSortSingleDoubleArray() {
+        Double[] unsortedArray = new Double[] { 1.0 };
+        Double[] expectedSortedArray = new Double[] { 1.0 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getFloatArrays() {
-        return Stream.of(
-            Arguments.of(new Float[] { 3.0f, 1.0f, 4.0f, 1.0f, 5.0f, 9.0f, 2.0f, 6.0f, 5.0f }, new Float[] { 1.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 5.0f, 6.0f, 9.0f }),
-            Arguments.of(new Float[] { }, new Float[] { }),
-            Arguments.of(new Float[] { 1.0f }, new Float[] { 1.0f }),
-            Arguments.of(new Float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f }, new Float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f }),
-            Arguments.of(new Float[] { 9.0f, 8.0f, 7.0f, 6.0f, 5.0f, 4.0f, 3.0f, 2.0f, 1.0f }, new Float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f }),
-            Arguments.of(new Float[] { Float.MAX_VALUE, 0.0f, -Float.MAX_VALUE }, new Float[] { -Float.MAX_VALUE, 0.0f, Float.MAX_VALUE }),
-            Arguments.of(new Float[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f }, new Float[] { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f }),
-            Arguments.of(new Float[] { -1.0f, -2.0f, -3.0f, -4.0f, -5.0f, -6.0f, -7.0f, -8.0f, -9.0f }, new Float[] { -9.0f, -8.0f, -7.0f, -6.0f, -5.0f, -4.0f, -3.0f, -2.0f, -1.0f })
-        );
+    @Test
+    public void shouldSortUnsortedDoubleArray() {
+        Double[] unsortedArray = new Double[] { 3.0, 1.0, 4.0, 1.0, 5.0, 9.0, 2.0, 6.0, 5.0 };
+        Double[] expectedSortedArray = new Double[] { 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 6.0, 9.0 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getBooleanArrays() {
-        return Stream.of(
-            Arguments.of(new Boolean[] { true, false, true, false, true, false, true, false, true }, new Boolean[] { false, false, false, false, true, true, true, true, true }),
-            Arguments.of(new Boolean[] { }, new Boolean[] { }),
-            Arguments.of(new Boolean[] { true }, new Boolean[] { true }),
-            Arguments.of(new Boolean[] { true, true, true, true, true, true, true, true, true }, new Boolean[] { true, true, true, true, true, true, true, true, true }),
-            Arguments.of(new Boolean[] { false, false, false, false, false, false, false, false, false }, new Boolean[] { false, false, false, false, false, false, false, false, false })
-        );
+    @Test
+    public void shouldSortEmptyByteArray() {
+        Byte[] unsortedArray = new Byte[] { };
+        Byte[] expectedSortedArray = new Byte[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getCharacterArrays() {
-        return Stream.of(
-            Arguments.of(new Character[] { 'c', 'a', 'd', 'a', 'e', 'i', 'b', 'f', 'e' }, new Character[] { 'a', 'a', 'b', 'c', 'd', 'e', 'e', 'f', 'i' }),
-            Arguments.of(new Character[] { }, new Character[] { }),
-            Arguments.of(new Character[] { 'a' }, new Character[] { 'a' }),
-            Arguments.of(new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' }, new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' }),
-            Arguments.of(new Character[] { 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' }, new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' }),
-            Arguments.of(new Character[] { Character.MAX_VALUE, 'a', Character.MIN_VALUE }, new Character[] { Character.MIN_VALUE, 'a', Character.MAX_VALUE }),
-            Arguments.of(new Character[] { 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a' }, new Character[] { 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a' }),
-            Arguments.of(new Character[] { 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r' }, new Character[] { 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' })
-        );
+    @Test
+    public void shouldSortSingleByteArray() {
+        Byte[] unsortedArray = new Byte[] { 1 };
+        Byte[] expectedSortedArray = new Byte[] { 1 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    private static Stream<Arguments> getStringArrays() {
-        return Stream.of(
-            Arguments.of(new String[] { "c", "a", "d", "a", "e", "i", "b", "f", "e" }, new String[] { "a", "a", "b", "c", "d", "e", "e", "f", "i" }),
-            Arguments.of(new String[] { }, new String[] { }),
-            Arguments.of(new String[] { "a" }, new String[] { "a" }),
-            Arguments.of(new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i" }, new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i" }),
-            Arguments.of(new String[] { "i", "h", "g", "f", "e", "d", "c", "b", "a" }, new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i" }),
-            Arguments.of(new String[] { "a", "a", "a", "a", "a", "a", "a", "a", "a" }, new String[] { "a", "a", "a", "a", "a", "a", "a", "a", "a" }),
-            Arguments.of(new String[] { "z", "y", "x", "w", "v", "u", "t", "s", "r" }, new String[] { "r", "s", "t", "u", "v", "w", "x", "y", "z" })
-        );
+    @Test
+    public void shouldSortUnsortedByteArray() {
+        Byte[] unsortedArray = new Byte[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+        Byte[] expectedSortedArray = new Byte[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
-    @ParameterizedTest
-    @MethodSource({ "getIntegerArrays", "getDoubleArrays", "getByteArrays", "getShortArray", "getLongArrays", "getFloatArrays", "getBooleanArrays", "getCharacterArrays", "getStringArrays" })
-    public <T extends Comparable<T>> void shouldSortArray(T[] unsortedArray, T[] expectedSortedArray) {
+    @Test
+    public void shouldSortEmptyShortArray() {
+        Short[] unsortedArray = new Short[] { };
+        Short[] expectedSortedArray = new Short[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleShortArray() {
+        Short[] unsortedArray = new Short[] { 1 };
+        Short[] expectedSortedArray = new Short[] { 1 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedShortArray() {
+        Short[] unsortedArray = new Short[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+        Short[] expectedSortedArray = new Short[] { 1, 1, 2, 3, 4, 5, 5, 6, 9 };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortEmptyLongArray() {
+        Long[] unsortedArray = new Long[] { };
+        Long[] expectedSortedArray = new Long[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleLongArray() {
+        Long[] unsortedArray = new Long[] { 1L };
+        Long[] expectedSortedArray = new Long[] { 1L };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedLongArray() {
+        Long[] unsortedArray = new Long[] { 3L, 1L, 4L, 1L, 5L, 9L, 2L, 6L, 5L };
+        Long[] expectedSortedArray = new Long[] { 1L, 1L, 2L, 3L, 4L, 5L, 5L, 6L, 9L };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortEmptyFloatArray() {
+        Float[] unsortedArray = new Float[] { };
+        Float[] expectedSortedArray = new Float[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleFloatArray() {
+        Float[] unsortedArray = new Float[] { 1.0f };
+        Float[] expectedSortedArray = new Float[] { 1.0f };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedFloatArray() {
+        Float[] unsortedArray = new Float[] { 3.0f, 1.0f, 4.0f, 1.0f, 5.0f, 9.0f, 2.0f, 6.0f, 5.0f };
+        Float[] expectedSortedArray = new Float[] { 1.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 5.0f, 6.0f, 9.0f };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortEmptyBooleanArray() {
+        Boolean[] unsortedArray = new Boolean[] { };
+        Boolean[] expectedSortedArray = new Boolean[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleBooleanArray() {
+        Boolean[] unsortedArray = new Boolean[] { true };
+        Boolean[] expectedSortedArray = new Boolean[] { true };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedBooleanArray() {
+        Boolean[] unsortedArray = new Boolean[] { true, false, true, false, true, false, true, false, true };
+        Boolean[] expectedSortedArray = new Boolean[] { false, false, false, false, true, true, true, true, true };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortEmptyCharacterArray() {
+        Character[] unsortedArray = new Character[] { };
+        Character[] expectedSortedArray = new Character[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleCharacterArray() {
+        Character[] unsortedArray = new Character[] { 'a' };
+        Character[] expectedSortedArray = new Character[] { 'a' };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedCharacterArray() {
+        Character[] unsortedArray = new Character[] { 'c', 'a', 'd', 'a', 'e', 'i', 'b', 'f', 'e' };
+        Character[] expectedSortedArray = new Character[] { 'a', 'a', 'b', 'c', 'd', 'e', 'e', 'f', 'i' };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortEmptyStringArray() {
+        String[] unsortedArray = new String[] { };
+        String[] expectedSortedArray = new String[] { };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortSingleStringArray() {
+        String[] unsortedArray = new String[] { "a" };
+        String[] expectedSortedArray = new String[] { "a" };
+
+        assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
+    }
+
+    @Test
+    public void shouldSortUnsortedStringArray() {
+        String[] unsortedArray = new String[] { "c", "a", "d", "a", "e", "i", "b", "f", "e" };
+        String[] expectedSortedArray = new String[] { "a", "a", "b", "c", "d", "e", "e", "f", "i" };
+
         assertArrayEquals(expectedSortedArray, getSortingAlgorithm().sort(unsortedArray));
     }
 
