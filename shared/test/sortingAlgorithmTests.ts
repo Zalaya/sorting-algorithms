db0ae0a66a961f67f61d49b9db1504c682387c73
Zalaya <enol.corzal@gmail.com>
2024-10-12 23:41:48 +0200

    feat(test): add more tests to the sorting algorithm tests function with jest

--- shared/test/sortingAlgorithmTests.ts
+++ shared/test/sortingAlgorithmTests.ts
@@ -32,6 +32,34 @@ export function sortingAlgorithmTests(sortingAlgorithm: SortingAlgorithm): void 
             expect(sortingAlgorithm.sort(unsortedArray)).toEqual(expectedArray);
         });
 
+        it("should sort an already sorted array", () => {
+            const unsortedArray: number[] = [ 1, 2, 3, 4, 5 ];
+            const expectedArray: number[] = [ 1, 2, 3, 4, 5 ];
+
+            expect(sortingAlgorithm.sort(unsortedArray)).toEqual(expectedArray);
+        });
+
+        it("should sort an array in descending order", () => {
+            const unsortedArray: number[] = [ 5, 4, 3, 2, 1 ];
+            const expectedArray: number[] = [ 1, 2, 3, 4, 5 ];
+
+            expect(sortingAlgorithm.sort(unsortedArray)).toEqual(expectedArray);
+        });
+
+        it("should sort an array with negative elements", () => {
+            const unsortedArray: number[] = [ -5, -3, -2, -8, -7, -4, -1, -9, -6 ];
+            const expectedArray: number[] = [ -9, -8, -7, -6, -5, -4, -3, -2, -1 ];
+
+            expect(sortingAlgorithm.sort(unsortedArray)).toEqual(expectedArray);
+        });
+
+        it("should sort an array with positive and negative elements", () => {
+            const unsortedArray: number[] = [ -5, 3, -2, 8, -7, 4, -1, 9, -6 ];
+            const expectedArray: number[] = [ -7, -6, -5, -2, -1, 3, 4, 8, 9 ];
+
+            expect(sortingAlgorithm.sort(unsortedArray)).toEqual(expectedArray);
+        });
+
     })
 
 }
