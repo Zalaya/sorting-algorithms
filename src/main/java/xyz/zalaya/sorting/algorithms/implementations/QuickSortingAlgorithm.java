f816367168e20228e776229700d49392d603ec2d
Zalaya <enol.corzal@gmail.com>
2024-10-16 23:13:05 +0200

    feat(algorithms): add the quick sorting algorithm and its tests

---
+++ src/main/java/xyz/zalaya/sorting/algorithms/implementations/QuickSortingAlgorithm.java
@@ -0,0 +1,58 @@
+package xyz.zalaya.sorting.algorithms.implementations;
+
+import xyz.zalaya.sorting.algorithms.SortingAlgorithm;
+
+import static xyz.zalaya.sorting.utilities.ArrayUtilities.swap;
+
+/**
+ * Implementation of the quick sorting algorithm.
+ * It implements the {@link SortingAlgorithm} interface.
+ */
+public class QuickSortingAlgorithm implements SortingAlgorithm {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        pass(array, 0, array.length - 1);
+
+        return array;
+    }
+
+    /**
+     * Perform a pass of the quick sorting algorithm.
+     * @param array The array to sort.
+     * @param startIndex The index to start sorting from.
+     * @param endIndex The index to end sorting at.
+     */
+    private <T extends Comparable<T>> void pass(T[] array, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            int partitionIndex = partition(array, startIndex, endIndex);
+
+            pass(array, startIndex, partitionIndex - 1);
+            pass(array, partitionIndex + 1, endIndex);
+        }
+    }
+
+    /**
+     * Partition the array by selecting a pivot and moving all elements less than the pivot to the left of it.
+     * @param array The array to partition.
+     * @param startIndex The index to start partitioning from.
+     * @param endIndex The index to end partitioning at.
+     * @return The partition index.
+     */
+    private <T extends Comparable<T>> int partition(T[] array, int startIndex, int endIndex) {
+        T pivot = array[endIndex];
+        int i = startIndex - 1;
+
+        for (int j = startIndex; j < endIndex; j++) {
+            if (array[j].compareTo(pivot) < 0) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, endIndex);
+
+        return i + 1;
+    }
+
+}
