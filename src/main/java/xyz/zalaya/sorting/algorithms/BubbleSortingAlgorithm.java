e7f57add0b055bb74102a168eeaee8dbb70e4f24
Zalaya <enol.corzal@gmail.com>
2024-10-27 14:24:58 +0100

    refactor(bubble): change the pass method to an iteration one and upgrade the algorithm

--- src/main/java/xyz/zalaya/sorting/algorithms/BubbleSortingAlgorithm.java
+++ src/main/java/xyz/zalaya/sorting/algorithms/BubbleSortingAlgorithm.java
@@ -11,7 +11,7 @@ public class BubbleSortingAlgorithm implements SortingAlgorithm {
     @Override
     public <T extends Comparable<T>> T[] sort(T[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            if (!pass(array, i)) {
+            if (!iteration(array, i)) {
                 break;
             }
         }
@@ -20,37 +20,22 @@ public class BubbleSortingAlgorithm implements SortingAlgorithm {
     }
 
     /**
-     * Perform a pass of the bubble sorting algorithm.
+     * Perform an iteration of the bubble sorting algorithm.
      * @param array The array to sort.
      * @param startIndex The index to start sorting from.
      * @return True if a swap was performed, false otherwise.
      */
-    private <T extends Comparable<T>> boolean pass(T[] array, int startIndex) {
+    private <T extends Comparable<T>> boolean iteration(T[] array, int startIndex) {
         boolean swapped = false;
 
         for (int i = 0; i < array.length - startIndex - 1; i++) {
-            if (performSwapIfNeeded(array, i, i + 1)) {
+            if (array[i].compareTo(array[i + 1]) > 0) {
+                swap(array, i, i + 1);
                 swapped = true;
             }
         }
 
         return swapped;
-    }
-
-    /**
-     * Swap two elements in the array if the first element is greater than the second element.
-     * @param array The array to swap elements in.
-     * @param firstIndex The index of the first element.
-     * @param secondIndex The index of the second element.
-     * @return True if the elements were swapped, false otherwise.
-     */
-    private <T extends Comparable<T>> boolean performSwapIfNeeded(T[] array, int firstIndex, int secondIndex) {
-        if (array[firstIndex].compareTo(array[secondIndex]) > 0) {
-            swap(array, firstIndex, secondIndex);
-            return true;
-        }
-
-        return false;
     }
 
     /**
