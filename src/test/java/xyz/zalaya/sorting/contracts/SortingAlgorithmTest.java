764affbabf5f2204bd906ea6733bd4ff63c7e291
Zalaya <enol.corzal@gmail.com>
2024-10-27 17:01:51 +0100

    refactor(structure): upgrade the project structure and prepare it for the documentation

--- src/test/java/xyz/zalaya/sorting/contracts/SortingAlgorithmTest.java
+++ src/test/java/xyz/zalaya/sorting/contracts/SortingAlgorithmTest.java
@@ -3,7 +3,6 @@ package xyz.zalaya.sorting.contracts;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import xyz.zalaya.sorting.contracts.SortingAlgorithm;
 
 import java.util.Arrays;
 import java.util.List;
